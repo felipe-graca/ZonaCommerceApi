7f2134dba626d6a5591c7c1d7c6b619aa314b1a0
Felipe D'Oliveira Graça <69287434+Felipegraca-1999@users.noreply.github.com>
2021-10-01 14:51:56 -0300

    Projeto renomeado para ZonaCommerceApi

---
+++ ZonaCommerceApi/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZonaCommerceApi
+{
+    public class Produto
+    {
+        [Key]
+        public int idProduto { get; set; }
+        public string nomeProduto { get; set; }
+        public double valorProduto { get; set; }
+        public string urlImg { get; set; }
+
+    }
+}

4473c352d5fe814cdeb2abaa81d7e1ec2865c746
Felipe D'Oliveira Graça <69287434+Felipegraca-1999@users.noreply.github.com>
2021-10-01 14:30:05 -0300

    config gitignore

---
+++ JetCommerceApi/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JetCommerceApi
+{
+    public class Produto
+    {
+        [Key]
+        public int idProduto { get; set; }
+        public string nomeProduto { get; set; }
+        public double valorProduto { get; set; }
+        public string urlImg { get; set; }
+
+    }
+}

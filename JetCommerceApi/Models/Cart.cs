7b8dbbaa9f788a74f87cf7240c7a766273754836
Felipe D'Oliveira Graça <69287434+Felipegraca-1999@users.noreply.github.com>
2021-10-01 14:33:01 -0300

    config project

---
+++ JetCommerceApi/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace JetCommerceApi
+{
+    public class Cart
+    {
+        [Key]
+        public int idItem { get; set; }
+        public int idProduto { get; set; }
+        public int quantidadeProduto { get; set; }
+        public double valorProduto { get; set; }
+
+    }
+}
